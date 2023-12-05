7aee2bc8910158ff0d344ecb052ba5ecf94c0934
Dmitriy199708 <119492971+Dmitriy199708@users.noreply.github.com>
2023-12-05 20:05:11 +0300

    add CashBackHacker

---
+++ src/main/java/ru/netology/CashBackHacker.java
@@ -0,0 +1,14 @@
+package ru.netology;
+
+public class CashBackHacker {
+    private final int boundary = 1000;
+
+    public int remain(int amount) {
+        return boundary - amount % boundary;
+    }
+    public int getBoundary() {
+        return boundary;
+    }
+
+}
+
