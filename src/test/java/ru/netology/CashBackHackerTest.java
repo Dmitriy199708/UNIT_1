625ca9ddecf1f0e3b731cfaa2210d4f194226c92
Dmitriy199708 <119492971+Dmitriy199708@users.noreply.github.com>
2024-01-07 21:16:37 +0300

    edit testng

--- src/test/java/ru/netology/CashBackHackerTest.java
+++ src/test/java/ru/netology/CashBackHackerTest.java
@@ -1,34 +1,31 @@
 package ru.netology;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
-import org.junit.Assert;
-import org.junit.Test;
+import static org.testng.Assert.*;
 
 public class CashBackHackerTest {
-    @Test
-    public void cashbackAccrualWhenPurchasingFor900(){
+    @org.testng.annotations.Test
+    public void cashbackAccrualWhenPurchasingFor900() {
 
         CashBackHacker service = new CashBackHacker();
 
         int actual = service.remain(900);
         int expected = 100;
-
-        Assert.assertEquals(actual,expected);
+        assertEquals(actual, expected);
     }
 
-    @Test
-    public void cashbackAccrualWhenPurchasingFor1000(){
-
+    @org.testng.annotations.Test
+    public void cashbackAccrualWhenPurchasingFor1000() {
         CashBackHacker service = new CashBackHacker();
-
-
         int actual = service.remain(1000);
-        int expected = 0;
+        int expected = 1000;
 
-        Assert.assertEquals(actual,expected);
+        assertEquals(actual, expected);
     }
-    @Test
-    public void cashbackAccrualWhenPurchasingFor1100(){
+    @org.testng.annotations.Test
+    public void cashbackAccrualWhenPurchasingFor1100() {
 
         CashBackHacker service = new CashBackHacker();
 
@@ -36,10 +33,11 @@ public class CashBackHackerTest {
         int actual = service.remain(1000);
         int expected = 1000;
 
-        Assert.assertEquals(actual,expected);
+        assertEquals(actual, expected);
     }
-    @Test
-    public void cashbackAccrualWhenPurchasingFor999(){
+
+    @org.testng.annotations.Test
+    public void cashbackAccrualWhenPurchasingFor999() {
 
         CashBackHacker service = new CashBackHacker();
 
@@ -47,7 +45,7 @@ public class CashBackHackerTest {
         int actual = service.remain(999);
         int expected = 1;
 
-        Assert.assertEquals(actual,expected);
+        assertEquals(actual, expected);
     }
 
 }
